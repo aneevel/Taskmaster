39b535cff4827e2991e32c2db11e7249426f29eb
Alec Neevel <aneevel15@gmail.com>
2023-11-24 14:39:47 -0600

    Add top-level components

---
+++ src/app/one-off/one-off.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OneOffComponent } from './one-off.component';
+
+describe('OneOffComponent', () => {
+  let component: OneOffComponent;
+  let fixture: ComponentFixture<OneOffComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [OneOffComponent]
+    });
+    fixture = TestBed.createComponent(OneOffComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
