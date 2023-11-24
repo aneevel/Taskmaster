b69bc71cf80b60ffd19918d56c1d719d46c205a5
Alec Neevel <aneevel15@gmail.com>
2023-11-24 14:48:15 -0600

    Add all route connections

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
 import { DailyComponent } from './daily/daily.component';
+import { WeeklyComponent } from './weekly/weekly.component';
+import { MonthlyComponent } from './monthly/monthly.component';
+import { OneOffComponent } from './one-off/one-off.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { RegisterComponent } from './register/register.component';
 
 const routeConfig: Routes = [
     {
@@ -12,6 +19,41 @@ const routeConfig: Routes = [
       path: 'daily',
       component: DailyComponent,
       title: 'Daily Tasks'
+    },
+    {
+      path: 'weekly',
+      component: WeeklyComponent,
+      title: 'Weekly Tasks'
+    },
+    {
+      path: 'monthly',
+      component: MonthlyComponent,
+      title: 'Monthly Tasks'
+    },
+    {
+      path: 'one-off',
+      component: OneOffComponent,
+      title: 'One-Off Tasks'
+    },
+    {
+      path: 'account',
+      component: AccountComponent,
+      title: 'Account Overview'
+    },
+    {
+      path: 'login',
+      component: LoginComponent,
+      title: 'Login'
+    },
+    {
+      path: 'logout',
+      component: LogoutComponent,
+      title: 'Logout'
+    },
+    {
+      path: 'register',
+      component: RegisterComponent,
+      title: 'Register an Account'
     }
 ]
 
