2f780706e3e660df1cc1abe8f9e6bff87ab7e217
Alec Neevel <aneevel15@gmail.com>
2023-11-25 21:26:05 -0600

    Write tests for navigation elements existing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,8 +4,7 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule, AppComponent],
   }));
 
   it('should create the app', () => {
@@ -18,12 +17,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Taskmaster');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Taskmaster app is running!');
   });
 });
--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +10,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      imports: [ RouterTestingModule, HeaderComponent]
     });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -17,5 +19,34 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have items for home, daily, weekly, monthly, one-off, account, login, logout, and register', () => {
+    const homeElement = fixture.debugElement.query(By.css('#nav-home'));
+    expect(homeElement).toBeTruthy();
+
+    const dailyElement = fixture.debugElement.query(By.css('#nav-daily'));
+    expect(dailyElement).toBeTruthy();
+
+    const weeklyElement = fixture.debugElement.query(By.css('#nav-weekly'));
+    expect(weeklyElement).toBeTruthy();
+
+    const monthlyElement = fixture.debugElement.query(By.css('#nav-monthly'));
+    expect(monthlyElement).toBeTruthy();
+
+    const oneOffElement = fixture.debugElement.query(By.css('#nav-one-off'));
+    expect(oneOffElement).toBeTruthy();
+
+    const accountElement = fixture.debugElement.query(By.css('#nav-account'));
+    expect(accountElement).toBeTruthy();
+
+    const loginElement = fixture.debugElement.query(By.css('#nav-login'));
+    expect(loginElement).toBeTruthy();
+
+    const logoutElement = fixture.debugElement.query(By.css('#nav-logout'));
+    expect(logoutElement).toBeTruthy();
+
+    const registerElement = fixture.debugElement.query(By.css('#nav-register'));
+    expect(registerElement).toBeTruthy();
   });
 });
