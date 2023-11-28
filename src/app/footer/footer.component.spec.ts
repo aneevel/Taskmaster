6e1dc4e6cb909c95e570703283455a26658a63c2
Alec Neevel <aneevel15@gmail.com>
2023-11-27 20:48:58 -0600

    Add footer tests

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { By } from '@angular/platform-browser';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -17,5 +18,17 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have anchor items for portfolio, github repo', () => {
+    const portfolioElement = fixture.debugElement.query(By.css('#footer-portfolio'));
+    expect(portfolioElement).toBeTruthy();
+
+    const githubElement = fixture.debugElement.query(By.css('#footer-github'));
+    expect(githubElement).toBeTruthy();
   });
 });
