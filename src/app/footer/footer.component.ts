a0c0fb9385b9c5d8ad63fe4ecbb8000ca6f2174f
Alec Neevel <aneevel15@gmail.com>
2023-11-27 20:57:16 -0600

    Add links for portfolio and github

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  imports: [
+    ToolbarModule,
+    ButtonModule,
+    RouterModule
+  ]
 })
 export class FooterComponent {
 
