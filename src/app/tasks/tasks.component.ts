2df04f8ff0fd55fed35f10f462dc332b49fc3ed0
Alec Neevel <aneevel15@gmail.com>
2023-12-06 20:52:39 -0600

    create tasks component

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.scss']
+})
+export class TasksComponent {
+
+}
