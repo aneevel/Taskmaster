d3aea31abeb71b1bd7ca7803ce22d8ee5e5abe0a
Alec Neevel <aneevel15@gmail.com>
2023-12-06 20:42:51 -0600

    include footer in main display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
 import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 import { RouterModule } from "@angular/router";
+import { Footer } from "primeng/api";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HomeComponent, RouterModule, HeaderComponent],
+  imports: [HomeComponent, 
+    RouterModule, 
+    HeaderComponent, 
+    FooterComponent],
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
