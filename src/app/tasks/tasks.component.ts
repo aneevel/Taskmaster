40c8bae3565621b2c3e0e4a4c44ca8785f5f02ef
Alec Neevel <aneevel15@gmail.com>
2023-12-19 16:08:52 -0600

    dynamically add create-task modal

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { CreateTaskComponent } from '../create-task/create-task.component';
 
 @Component({
   standalone: true,
@@ -10,7 +11,10 @@ import { ButtonModule } from 'primeng/button';
 })
 export class TasksComponent {
 
+  @ViewChild("createTaskContainer", { read: ViewContainerRef }) createTaskContainer: any;
+
   openCreateTask() {
-    console.log("Create new task")
+    this.createTaskContainer.clear();
+    const createTaskModal = this.createTaskContainer.createComponent(CreateTaskComponent)
   }
 }
