5df505e5566fc93d59ebd8e94c1a51af8a1c9c94
Alec Neevel <aneevel15@gmail.com>
2023-12-28 09:59:50 -0600

    add priority dropdown

--- src/app/create-task/create-task.component.ts
+++ src/app/create-task/create-task.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext'
+import { InputTextareaModule } from 'primeng/inputtextarea'
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   standalone: true,
   selector: 'app-create-task',
+  imports: [ DropdownModule],
   templateUrl: './create-task.component.html',
   styleUrls: ['./create-task.component.scss'],
 })
