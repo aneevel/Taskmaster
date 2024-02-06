afca428edf2731635496fc4fa9a23fd3569116b4
Alec Neevel <aneevel15@gmail.com>
2024-02-06 16:00:38 -0600

    pass test for constructing create-task component

--- src/app/create-task/create-task.component.spec.ts
+++ src/app/create-task/create-task.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateTaskComponent } from './create-task.component';
 import { By } from '@angular/platform-browser';
+import { DynamicDialogComponent, DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 describe('CreateTaskComponent', () => {
   let component: CreateTaskComponent;
@@ -9,7 +10,9 @@ describe('CreateTaskComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [CreateTaskComponent]
+      imports: [CreateTaskComponent],
+      providers: [DynamicDialogRef, 
+        DynamicDialogConfig]
     });
     fixture = TestBed.createComponent(CreateTaskComponent);
     component = fixture.componentInstance;
