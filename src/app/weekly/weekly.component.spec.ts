5d0421b81b267244f3e21a3005955904ee2f4f7b
Alec Neevel <aneevel15@gmail.com>
2024-02-13 15:35:55 -0600

    remove unnecessary test

--- src/app/weekly/weekly.component.spec.ts
+++ src/app/weekly/weekly.component.spec.ts
@@ -22,10 +22,13 @@ describe("WeeklyComponent", () => {
 
   it("should only have weekly tasks", () => {
 
+    // Create a couple tasks, some not weekly
+    
+
     // If there are any non-weekly tasks, they can be grabbed by any
     // occurrence class other than "Weekly" - the existence of just one
     // is a failure
-    const notMatchingTask = fixture.debugElement.query(By.css('app-task-item:not.Weekly'));
+    const notMatchingTask = fixture.debugElement.query(By.css('app-task-item'));
     expect(notMatchingTask).toBeFalsy();
   });
 });
