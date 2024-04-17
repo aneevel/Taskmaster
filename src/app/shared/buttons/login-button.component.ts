0ac6cb6f2bb9099d3636e34f98109479c7aac477
Alec Neevel <aneevel15@gmail.com>
2024-04-17 11:30:57 -0500

    remove auth0 code

--- src/app/shared/buttons/login-button.component.ts
+++ src/app/shared/buttons/login-button.component.ts
@@ -1,24 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-login-button',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <button class="button__login" (click)="handleLogin()">Log In</button>
-  `
+  template: ``
 })
 export class LoginButtonComponent {
-  constructor(private auth: AuthService) {}
 
-  handleLogin(): void {
-    this.auth.loginWithRedirect({
-      appState: {
-        // Redirects users to home on login, but perhaps it should be an user setting?
-        target: '/',
-      },
-    });
-  }
 }
