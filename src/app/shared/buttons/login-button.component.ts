c7d4685e456d72e338164e865eae635de34f78a4
Alec Neevel <aneevel15@gmail.com>
2024-04-19 14:53:50 -0500

    fix routerlink for login button

--- src/app/shared/buttons/login-button.component.ts
+++ src/app/shared/buttons/login-button.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login-button',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <button>
         <a class='nav-link'
-            routerLink='/login'
-            routerLinkActive='active'>
+            [routerLink]="['/login']">
             Log In
         </a>
     </button>
