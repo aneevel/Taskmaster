08eeb1fe2024fdfffd231d5f69ce1163868f671b
aneevel <aneevel15@gmail.com>
2024-07-25 21:02:10 -0500

    setup logging with morgan

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
 
 const db = require('./data/database');
 
 const userRoutes = require('./routes/users-routes');
 
 app.use(bodyParser.json());
+
+app.use(morgan('combined'));
+
+app.use(morgan('dev', {
+    stream: fs.createWriteStream(path.join(__dirname, 'logs', 'error.log'), { flags: 'a' }),
+    skip: function (req, res) { return res.statusCode < 400 }
+}));
+
+app.use(morgan('common', {
+    stream: fs.createWriteStream(path.join(__dirname, 'logs', 'access.log'), { flags: 'a' })
+}));
 
 app.get('/api', function (req, res) { return res.send('Hello from Express!'); });
 
