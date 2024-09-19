68cf3c8be64ea6b714e78964c0ea6fb68a1e5d7d
aneevel <aneevel15@gmail.com>
2024-09-19 14:37:26 -0500

    add authGuard function

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,9 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from '../user.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+    return inject(UserService).isAuthenticated()
+        ? true 
+        : inject(Router).createUrlTree(['/login']);
+};
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,6 +7,7 @@ import { OneOffComponent } from "./one-off/one-off.component";
 import { AccountComponent } from "./account/account.component";
 import { UsersComponent } from "./user/users/users.component";
 import { LoginComponent } from "./login/login.component";
+import { authGuard } from "./guards/auth.guard";
 
 const routeConfig: Routes = [
   {
@@ -22,26 +23,31 @@ const routeConfig: Routes = [
     path: "daily",
     component: DailyComponent,
     title: "Daily Tasks",
+    canActivate: [authGuard]
   },
   {
     path: "weekly",
     component: WeeklyComponent,
     title: "Weekly Tasks",
+    canActivate: [authGuard]
   },
   {
     path: "monthly",
     component: MonthlyComponent,
     title: "Monthly Tasks",
+    canActivate: [authGuard]
   },
   {
     path: "one-off",
     component: OneOffComponent,
     title: "One-Off Tasks",
+    canActivate: [authGuard]
   },
   {
     path: "account",
     component: AccountComponent,
     title: "Account Overview",
+    canActivate: [authGuard]
   },
   { 
     path: "login",
@@ -52,6 +58,7 @@ const routeConfig: Routes = [
   {
     path: 'users',
     component: UsersComponent,
+    title: "Users Overview"
   }
 
 ];
