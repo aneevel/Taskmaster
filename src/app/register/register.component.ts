844078486414c04901070708af3b43ede32dc983
aneevel <aneevel15@gmail.com>
2024-09-22 15:35:36 -0500

    add register form getter

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
 
 @Component({
-  selector: 'app-register',
+  selector: 'app-register', standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+    registerForm: FormGroup = this.formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required],
+    });
+    loading = false;
+    submitted = false;
+    returnUrl: string;
 
+    constructor(
+        private formBuilder: FormBuilder,
+        private userService: UserService,
+        private route: ActivatedRoute,
+        private router: Router 
+    ) {
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+
+    get form() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        if (this.registerForm.invalid) {
+            return;
+        }
+    }
 }
