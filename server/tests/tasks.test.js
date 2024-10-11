b7e8dc3d2e35b08ac922f2204436c403851d1be1
aneevel <aneevel15@gmail.com>
2024-10-11 16:09:28 -0500

    template out tests

--- server/tests/tasks.test.js
+++ server/tests/tasks.test.js
@@ -1,61 +1,244 @@
-const request = require('supertest');
-const app = require('../server.js');
-const Task = require('../models/task-model.js');
-
-describe('POST /tasks/new', () => {
-    it('should return a 400 error when providing an empty body', async () => {
-        const response = await request(app).post('/tasks/new').send({});
-        expect(response.status).toBe(400);
-        expect(response.body).toHaveProperty("message", "Improper params supplied");
-    });
-
-    it('should return a 400 error when provided an incorrect amount of params', async () => {
-       const task = new Task(
-            "Test Description",
-            "High",
-            Date.now(),
-            "Daily",
-        );
-
-        const response = await request(app).post('/tasks/new').send(task);
-        expect(response.status).toBe(400);
-        expect(response.body).toHaveProperty("message", "Improper params supplied");
-    });
-
-    it('should return a 200 code when provided the correct params', async () => {
-       const task = new Task(
-            "Test Description",
-            "High",
-            Date.now(),
-            "Daily",
-            1
-        );
+describe('Tasks', () => {
+    
+    describe('GET All Tasks', () => {
         
-        const response = await request(app).post('/tasks/new').send(task);
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty("message", "Task created");
-        expect(response.body).toHaveProperty("id");
+        describe('Given tasks do not exists', () => {
+            
+            it('Should return a 200 code', () => {
+
+            });
+
+            it('Should return an empty collection of tasks', () => {
+
+            });
+        });
+
+        describe('Given tasks do exist', () => {
+
+            it('Should a return a 200 code', () => {
+
+            });
+
+            it('Should return a collection of valid tasks', () => {
+
+            });
+        });
     });
-});
 
-describe('POST /tasks', () => {
-   it('should return a collection of tasks with status 200', async () => {
-       const response = await request(app).get('/tasks');
-       expect(response.status).toBe(200);
-       expect(response.body).toEqual(expect.any(Array));
-   });
-});
+    describe('GET Task with ID', () => {
 
-describe('POST/tasks/:id', () => {
-    it('should return a task with status 200 when provided a valid user ID', async () => {
-        const response = await request.app.get('/tasks/66f1bb3a4ac06f4c3bde5457');
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual(expect.any(Task));
+        describe('Given task with ID does not exist', () => {
+
+            it('Should return a 400 code', () => {
+
+            });
+
+            it('Should return an error message stating task does not exist', () => {
+
+            });
+        });
+
+        describe('Given task with ID does exist', () => {
+
+            describe('Given request does not have a valid JWT Token', () => {
+
+                it('Should return a 401 code', () => {
+
+                });
+
+                it('Should return an error message stating request does not have authentication', () => {
+
+                });
+            });
+
+            describe('Given request does have a valid JWT Token', () => {
+
+                it('Should return a 200 code', () => {
+                    
+                });
+
+                it('Should return a valid task matching ID', () => {
+
+                });
+
+            });
+        });
     });
 
-    it('should return a 400 error code with error message when provided a nonexistent user ID', async () => {
-        const response = await request.app.get('/tasks/000000000000000000000000');
-        expect(response.status).toBe(400);
-        expect(response.body).toHaveProperty("message", "No user with ID");
+    describe('POST Create New Task', () => {
+        
+        describe('Given request does not have a valid JWT Token', () => {
+            
+            it('Should return a 401 code', () => {
+
+            });
+
+            it('Should return an error message stating request does not have authentication', () => {
+
+            });
+
+        });
+
+        describe('Given request does have a valid JWT Token', () => {
+
+            describe('Given request does not provide a body of params', () => {
+
+                it('Should return a 400 code', () => {
+
+                });
+
+                it('Should return an error message stating request did not provide params', () => {
+
+                });
+            });
+
+            describe('Given request does not provide the proper number of params', () => {
+                
+                it('Should return a 400 code', () => {
+
+                });
+
+                it('Should return an error message stating request did not provide the specified number of params', () => {
+
+                });
+            });
+
+            describe('Given request does not provide valid params', () => {
+                
+                describe('Given request has malformed description', () => {
+                   
+                    describe('Given request has empty description', () => {
+                        
+                        it('Should return a 400 code', () => {
+
+                        });
+
+                        it('Should return an error message stating tasks must have a non-empty description', () => {
+
+                        });
+                    });
+
+                    describe('Given request has a description exceeding maximum length', () => {
+
+                        it('Should return a 400 code', () => {
+
+                        });
+
+                        // TODO: What is the maximum length?
+                        it('Should return an error message stating task description has exceed maximum length of ???', () => {
+
+                        });
+                    });
+                });
+
+                describe('Given request has a malformed priority', () => {
+                    
+                    describe('Given request has empty priority', () => {
+
+                        it('Should return a 400 code', () => {
+
+                        });
+
+                        it('Should return an error message stating tasks must have a non-empty priority', () => {
+
+                        });
+                    });
+
+                    describe('Given request has a priority of non-existent type', () => {
+                        
+                        it('Should return a 400 code', () => {
+
+                        });
+
+                        it('Should return an error message stating priority must be an existing priority type', () => {
+
+                        });
+                    });
+                });
+
+                describe('Given request has a malformed due date', () => {
+
+                    describe('Given request has an empty date', () => {
+
+                        it('Should return a 400 code', () => {
+
+                        });
+
+                        it('Should return an error message stating tasks must have a due date', () => {
+
+                        });
+                    });
+
+                    describe('Given request has an incorrectly formed date', () => {
+
+                        it('Should return a 400 code', () => {
+
+                        });
+
+                        it('Should return an error message stating tasks must have a properly formed due date', () => {
+
+                        });
+                    });
+
+                    describe('Given request has a date that has already passed', () => {
+
+                        it('Should return a 400 code', () => {
+                            
+                        });
+
+                        it('Should return an error message stating tasks must have a due date that has not already occurred', () => {
+
+                        });
+                    });
+
+                describe('Given request has a malformed occurrence', () => {
+                
+                    describe('Given request has an empty occurrence', () => {
+                    
+                        it('Should return a 400 code', () => {
+
+                        });
+
+                        it('Should return an error message stating tasks must have a non-empty occurrence', () => {
+
+                        });
+                    });
+
+                    describe('Given request has a non-matching occurrence type', () => {
+                        
+                        it('Should return a 400 code', () => {
+
+                        });
+
+                        it('Should return an error message stating tasks must have a valid occurrence type', () => {
+
+                        });
+                    });
+                });
+
+                describe('Given request has a non-existing userID', () => {
+                    
+                    it('Should return a 400 code', () => {
+                    
+                    });
+
+                    it('Should return an error message stating that no user exists for userID', () => {
+
+                    });
+                });
+            });
+
+            describe('Given request is a valid Task', () => {
+                
+                it('Should return a 200 code', () => {
+
+                });
+
+                it('Should return a valid Task object', () => {
+
+                });
+            });
+        });
+    });
     });
 });
