b76bb6bb63b09e8c47f1fbcf64122bd2091b4832
aneevel <aneevel15@gmail.com>
2024-11-18 15:57:21 -0600

    write failing GET user test

--- server/tests/routes/users/unit.test.js
+++ server/tests/routes/users/unit.test.js
@@ -1,6 +1,8 @@
 const supertest = require('supertest');
 const app = require('../../../server');
 
+const existingUserID = '66a2c3b69d5dbcf506d743bb';
+
 describe('Users', () => {
 
     describe('GET ALL Users', () => {
@@ -13,6 +15,27 @@ describe('Users', () => {
                 .then((response) => {
                     expect(Array.isArray(response.body)).toBeTruthy();
                 });
+        });
+    });
+
+    describe('GET User with ID', () => {
+
+        describe('Given ID matches a valid and existing user ID', () => {
+            
+            it ('Should return a 200 code and an user object', async () => {
+                
+                await supertest(app)
+                    .get(`/users/${existingUserID}`)
+                    .expect(200)
+                    .then((response) => {
+                        expect(response.body.user).toHaveProperty('email');
+                        expect(response.body.user).toHaveProperty('password');
+                        expect(response.body.user).toHaveProperty('lname');
+                        expect(response.body.user).toHaveProperty('fname');
+                        expect(response.body.user).toHaveProperty('confirmed');
+                        expect(response.body.user).toHaveProperty('admin');
+                    });
+            });
         });
     });
 });
