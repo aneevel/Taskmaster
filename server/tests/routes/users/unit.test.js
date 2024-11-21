5738bf06828c33553b58232cf27354758d621114
aneevel <aneevel15@gmail.com>
2024-11-21 16:02:32 -0600

    create isValidEmailAddress helper function to test emails

--- server/tests/routes/users/unit.test.js
+++ server/tests/routes/users/unit.test.js
@@ -90,12 +90,28 @@ describe('Users', () => {
 
             describe('Given no email was provided', () => {
                 
-                it('Should return a 400 error and an error message stating an email must be specified', async () => {
+                it('Should return a 400 error and an error message stating a valid, non-existing email must be provided', async () => {
                     
                     await supertest(app)
                         .post(`/users/new`)
                         .send({
                             "email": ""
+                        })
+                        .expect(400)
+                        .then((response) => {
+                            expect(response.body.message).toBe("A valid, non-existing email must be provided");
+                        });
+                });
+            });
+
+            describe('Given an invalid email was provided', () => {
+                
+                it('Should return a 400 error and an error message stating a valid, non-existing email must be provided', async () => {
+                    
+                    await supertest(app)
+                        .post(`/users/new`)
+                        .send({
+                            "email": "test"
                         })
                         .expect(400)
                         .then((response) => {
--- server/utility/validation.js
+++ server/utility/validation.js
@@ -20,7 +20,14 @@ const emailIsConfirmed = (email, confirmEmail) => {
     return email === confirmEmail;
 }
 
+const isValidEmailAddress = (email) => {
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
 module.exports = {
     userDetailsAreValid: userDetailsAreValid,
-    emailIsConfirmed: emailIsConfirmed 
+    emailIsConfirmed: emailIsConfirmed,
+    isValidEmailAddress: isValidEmailAddress
 }
