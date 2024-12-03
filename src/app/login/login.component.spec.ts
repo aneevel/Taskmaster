54da2b8ffd8f9009b3fedf85f826628f8ec15259
aneevel <aneevel15@gmail.com>
2024-12-03 16:22:37 -0600

    write test for valid user service form

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from "@angular/router/testing";
 
+import { of } from 'rxjs';
+
 import { LoginComponent } from './login.component';
 import routes from "../routes";
 
@@ -22,5 +24,16 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should invoke user service when form is valid', () => {
+        const email = component.form['username'];
+        email.setValue('test@test.com');
+        const password = component.form['password'];
+        password.setValue('123456789');
+        userServiceStub.login.and.returnValue(of());
+
+        fixture.nativeElement.querySelector('button').click();
+        expect(userServiceStub.login.calls.any()).toBeTruthy();
   });
 });
--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -16,4 +16,5 @@ describe('UserService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
 });
