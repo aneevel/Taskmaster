d5838bbfa9fda3c1b2d7ae2fd14fcbf5e8ce9815
aneevel <aneevel15@gmail.com>
2025-01-01 15:21:54 -0600

    check health function throughout service lifespan

--- src/app/api-gateway.service.ts
+++ src/app/api-gateway.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -6,17 +6,38 @@ import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiGatewayService {
+export class ApiGatewayService implements OnDestroy {
 
     private currentStatus = 0;
     private statusSubject = new BehaviorSubject<number>(0);
     private status$ = this.statusSubject.asObservable();
+    private healthCheckInterval: any;
 
     public API_URL: string = environment.api.serverUrl;
 
     constructor(private http: HttpClient) {
         this.statusSubject = new BehaviorSubject<number>(this.currentStatus);
         this.status$ = this.statusSubject.asObservable();
+        
+        // Start health checks when service is created
+        this.startHealthCheck();
+    }
+
+    private startHealthCheck(interval: number = 30000) {
+        // Initial check
+        this.getAPIStatus().subscribe();
+        
+        // Set up recurring checks
+        this.healthCheckInterval = setInterval(() => {
+            this.getAPIStatus().subscribe();
+        }, interval);
+    }
+
+    ngOnDestroy() {
+        // Clean up interval when service is destroyed
+        if (this.healthCheckInterval) {
+            clearInterval(this.healthCheckInterval);
+        }
     }
 
     getAPIStatus(): Observable<number> {
