6da717210821358821a583363f1698cd5bbb1abc
aneevel <aneevel15@gmail.com>
2025-01-02 16:43:26 -0600

    provide better email validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -29,7 +29,11 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.email]],
+      username: ['', [
+        Validators.required, 
+        Validators.email,
+        Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$')
+      ]],
       fname: ['', Validators.required],
       lname: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(6)]],
