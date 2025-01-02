70e6b6acc7452380c8dbe92b31592a07ead36261
aneevel <aneevel15@gmail.com>
2025-01-02 15:04:04 -0600

    add isAuth checks to frontend buttons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -24,6 +24,7 @@ import { UserService } from "../user.service";
   ],
 })
 export class HeaderComponent {
+  isAuthenticated$ = this.userService.isAuthenticated$;
 
   constructor(private userService: UserService) {
 
