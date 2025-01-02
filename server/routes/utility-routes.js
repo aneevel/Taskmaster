918c815f287523830d4165dadde51f8ef2ea09e0
aneevel <aneevel15@gmail.com>
2025-01-01 19:35:43 -0600

    create utility route handler under api path

--- server/routes/utility-routes.js
+++ server/routes/utility-routes.js
@@ -1,1 +1,8 @@
- 
+const express = require('express');
+const router = express.Router();
+const { getHealthStatus } = require('../controllers/utility-controller');
+
+// Health check endpoint
+router.get('/health', getHealthStatus);
+
+module.exports = router; 
