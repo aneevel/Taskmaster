7e2dca3ba652ccb201b91d34e0d3116c92aabfbe
aneevel <aneevel15@gmail.com>
2025-01-03 15:23:56 -0600

    refactor user-controller to utilize new schema

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -58,88 +58,77 @@ const getAllUsers = async (req, res, next) => {
 }
 
 const getUserById = async (req, res, next) => {
-
-    let user;
     try {
-        user = await User.findById(req.params.id);
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "No user with ID exists"
+            });
+        }
+        res.status(200).json(user);
     } catch (error) {
-        return next(error);
+        next(error);
     }
-
-    if (user == null) {
-        return res.status(404).json({ message: "No user with ID exists" });
-    }
-
-    return res.status(200).send(user);
-}
+};
 
 const deleteUser = async (req, res, next) => {
-
     try {
-        await User.deleteByUserId(req.params.id);
+        const result = await User.deleteByUserId(req.params.id);
+        if (!result.deletedCount) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+        return res.status(204).send();
     } catch (error) {
-        return next(error);
+        next(error);
     }
-
-    return res.status(204).send();
-}
+};
 
 const updateUser = async (req, res, next) => {
-
-    if (Object.keys(req.body).length === 0) {
-        return res.status(204).send();
-    }
-
-    if (!ValidationUtil.isEmpty(req.body.email)) {
-        if (ValidationUtil.isValidEmailAddress(req.body.email)) {
-            let user = new User(req.body.email);
-            let compareUser = await user.getUserWithSameEmail();
-            let givenUser = await User.findById(req.params.id);
-            if (JSON.stringify(compareUser) != JSON.stringify(givenUser)) {
-                console.log(`Error: That email already exists`);
-                return res.status(400).json({message: "This email address is already in use" });
-            }
-        } else {
-            return res.status(400).json({message: "A valid email address must be provided" });
-        }
-    }
-
-    if (!ValidationUtil.isEmpty(req.body.password) && !ValidationUtil.isValidPassword(req.body.password)) {
-        return res.status(400).json({message: "A password greater than 8 characters and less than 50 characters must be provided" });
-    }
-
-    if (!ValidationUtil.isEmpty(req.body.lname) && !ValidationUtil.isValidName(req.body.lname)) {
-        return res.status(400).json({message: "A last name must not exceed 50 characters" });
-    }
-
-    if (!ValidationUtil.isEmpty(req.body.fname) && !ValidationUtil.isValidName(req.body.fname)) {
-        return res.status(400).json({message: "A first name must not exceed 50 characters" });
-    }
-
-    let user;
     try {
-        user = await User.findById(req.params.id);
+        if (Object.keys(req.body).length === 0) {
+            return res.status(204).json({
+                success: true,
+                message: "No changes requested"
+            });
+        }
+
+        if (req.body.email) {
+            if (!ValidationUtil.isValidEmail(req.body.email)) {
+                return res.status(400).json({
+                    success: false,
+                    message: "A valid email address must be provided"
+                });
+            }
+            const existingUser = await User.findByEmail(req.body.email);
+            if (existingUser && existingUser._id.toString() !== req.params.id) {
+                return res.status(400).json({
+                    success: false,
+                    message: "This email address is already in use"
+                });
+            }
+        }
+
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "No user with ID was found"
+            });
+        }
+
+        const updatedUser = await user.patch(req.body);
+        return res.status(200).json({
+            success: true,
+            user: updatedUser
+        });
     } catch (error) {
-        return next(error);
+        next(error);
     }
-
-    if (!user) {
-        return res.status(404).json({ message: "No user with ID was found" }); }
-
-    let createdUser = new User(
-        user.email,
-        user.password,
-        user.lname,
-        user.fname
-    );
-    
-    if (!ValidationUtil.isEmpty(req.body.password)) 
-        req.body.password = await bcrypt.hash(req.body.password, 12);
-
-    createdUser = await createdUser.patch(req.body, req.params.id);
-
-    return res.status(200).send({ user: createdUser });
-}
+};
 
 module.exports = {
     getAllUsers: getAllUsers,
