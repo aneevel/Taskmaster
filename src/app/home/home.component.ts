2ea29bd99fb95fee6f7e1eedd7355556b55bcc43
aneevel <aneevel15@gmail.com>
2025-01-10 17:00:49 -0600

    dynamically display task count

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
+import { TasksComponent } from "../tasks/tasks.component";
 
 @Component({
   selector: "app-home",
   standalone: true,
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
+  imports: [ TasksComponent ]
 })
 export class HomeComponent {}
